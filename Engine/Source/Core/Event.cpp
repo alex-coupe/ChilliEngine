30afd3ae4992a47691b46c591613e95e9ba856d7
Alex <acooupe@gmail.com>
2021-03-13 21:18:29 +0000

    Finishing Event Class

--- Engine/Source/Core/Event.cpp
+++ Engine/Source/Core/Event.cpp
@@ -1,57 +1,5 @@
 #include "Event.h"
 
-
-Engine::Core::EventType operator |(Engine::Core::EventType lhs, Engine::Core::EventType rhs)
-{
-	return static_cast<Engine::Core::EventType> (
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(lhs) |
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(rhs)
-		);
-}
-
-Engine::Core::EventType operator &(Engine::Core::EventType lhs, Engine::Core::EventType rhs)
-{
-	return static_cast<Engine::Core::EventType> (
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(lhs) &
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(rhs)
-		);
-}
-
-Engine::Core::EventType operator ^(Engine::Core::EventType lhs, Engine::Core::EventType rhs)
-{
-	return static_cast<Engine::Core::EventType> (
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(lhs) ^
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(rhs)
-		);
-}
-
-Engine::Core::EventType operator ~(Engine::Core::EventType rhs)
-{
-	return static_cast<Engine::Core::EventType> (
-		~static_cast<std::underlying_type<Engine::Core::EventType>::type>(rhs)
-		);
-}
-
-Engine::Core::EventType& operator |=(Engine::Core::EventType& lhs, Engine::Core::EventType rhs)
-{
-	lhs = static_cast<Engine::Core::EventType> (
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(lhs) |
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(rhs)
-		);
-
-	return lhs;
-}
-
-Engine::Core::EventType& operator &=(Engine::Core::EventType& lhs, Engine::Core::EventType rhs)
-{
-	lhs = static_cast<Engine::Core::EventType> (
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(lhs) &
-		static_cast<std::underlying_type<Engine::Core::EventType>::type>(rhs)
-		);
-
-	return lhs;
-}
-
 void Engine::Core::Event::ProcessFrame()
 {
 	if (m_eventBuffer.size() > 0)
@@ -59,7 +7,7 @@ void Engine::Core::Event::ProcessFrame()
 		TrimBuffer();
 		const auto& e = m_eventBuffer.front();
 		m_eventBuffer.pop();
-		TranslateEvent(e);
+		ReadEvent(e);
 		for (const auto& subscriber : m_Subscribers)
 		{
 			if (std::find(subscriber.first.begin(), subscriber.first.end(), static_cast<EventType>(e->msg)) != subscriber.first.end())
@@ -82,6 +30,11 @@ void Engine::Core::Event::TrimBuffer()
 	}
 }
 
+void Engine::Core::Event::ResetKeyboard()
+{
+	m_keys.reset();
+}
+
 Engine::Core::Event::Event(const std::shared_ptr<DependencyResolver<SubSystem>>& m_resolver)
 	: SubSystem(m_resolver)
 {
@@ -102,6 +55,11 @@ int Engine::Core::Event::GetHash()const
 	return static_cast<int>(SystemTypes::EventSystem);
 }
 
+bool Engine::Core::Event::GetInWindow() const
+{
+	return m_inWindow;
+}
+
 void Engine::Core::Event::ClearBuffer()
 {
 	while (!m_eventBuffer.empty())
@@ -118,23 +76,46 @@ void Engine::Core::Event::Subscribe(const std::vector<EventType>& types, std::fu
 	
 }
 
-void Engine::Core::Event::TranslateEvent(const EventData* const data_in )
+void Engine::Core::Event::ReadEvent(const EventData* const data_in )
 {
 	switch (static_cast<EventType>(data_in->msg))
 	{
 		case EventType::MouseMove:
 			m_mouseX = data_in->lparam & 0x0000FFFF;
 			m_mouseY = data_in->lparam >> 16;
+			if (!m_inWindow)
+			{
+				SetCapture(data_in->handle);
+				m_inWindow = true;
+			}
 			break;
 		case EventType::MouseWheel:
-			m_wheelDelta = GET_WHEEL_DELTA_WPARAM(data_in->wparam);
+			if (m_inWindow)
+				m_wheelDelta = GET_WHEEL_DELTA_WPARAM(data_in->wparam);
 			break;
-		case EventType::Character:
+		case EventType::LeftMousePressed:
+			if (m_inWindow)
+				m_leftMousePressed = true;
+			break;
+		case EventType::LeftMouseReleased:
+			if (m_inWindow)
+				m_leftMousePressed = false;
+			break;
+		case EventType::MouseLeave:
+			m_inWindow = false;
+			break;
+		case EventType::FocusLost:
+			ResetKeyboard();
+			break;
 		case EventType::KeyUp:
 		case EventType::SysKeyUp:
+			m_keycode = static_cast<unsigned char>(data_in->wparam);
+			m_keys[m_keycode] = false;
+			break;
 		case EventType::KeyDown:
 		case EventType::SysKeyDown:
 			m_keycode = static_cast<unsigned char>(data_in->wparam);
+			m_keys[m_keycode] = true;
 			break;
 		case EventType::WindowResized:
 			RECT rect;
@@ -160,6 +141,33 @@ const int64_t& Engine::Core::Event::GetScreenWidth() const
 const int64_t& Engine::Core::Event::GetScreenHeight() const
 {
 	return m_screenHeight;
+}
+
+bool Engine::Core::Event::GetMouseLeftPressed() const
+{
+	return m_leftMousePressed;
+}
+
+bool Engine::Core::Event::GetMouseRightPressed() const
+{
+	return m_rightMousePressed;
+}
+
+short Engine::Core::Event::GetMouseWheelDelta() const
+{
+	return m_wheelDelta;
+}
+
+bool Engine::Core::Event::GetKeyPressed(Key key) const
+{
+	auto code = static_cast<unsigned int>(key);
+	return m_keys[code];
+}
+
+bool Engine::Core::Event::GetKeyReleased(Key key) const
+{
+	auto code = static_cast<unsigned int>(key);
+	return m_keys[code];
 }
 
 void Engine::Core::Event::Push(const EventData* const e)
