815718288c2f7eca7715d521dcf10143396f3a58
Alex Coupe <alexander.coupe@dunnhumby.com>
2022-08-24 10:18:10 +0100

    Refactor to use cache instead of dynamic pointer casting

--- Engine/Source/Core/DependencyResolver.cpp
+++ Engine/Source/Core/DependencyResolver.cpp
@@ -1,4 +1,4 @@
 #include "DependencyResolver.h"
 
 std::vector<std::shared_ptr<Engine::Core::SubSystem>> Engine::Core::DependencyResolver::m_dependencies = {};
-std::map<const char*, size_t> Engine::Core::DependencyResolver::m_cache = {};
+std::map<int, size_t> Engine::Core::DependencyResolver::m_cache = {};
--- Engine/Source/Core/DependencyResolver.h
+++ Engine/Source/Core/DependencyResolver.h
@@ -11,45 +11,29 @@ namespace Engine::Core {
 	public:
 		template<typename U>
 		static std::shared_ptr<U> ResolveDependency()
-		{	
-			for (auto& item : m_dependencies)
-			{
-				if (std::shared_ptr<U> dependency = std::dynamic_pointer_cast<U>(item); dependency != nullptr)
-					return dependency;
-			}
-			CHILLI_WARN("Failed to resolve dependency {}");
-			return nullptr;
-		}
-
-		template<typename U>
-		static std::shared_ptr<U> ResolveDependency(const char* name)
 		{
-			for (auto& item : m_dependencies)
+			U type;
+			auto itr = m_cache.find(type.GetSystemType());
+			if (itr == m_cache.end()) 
 			{
-				if (std::shared_ptr<U> dependency = std::dynamic_pointer_cast<U>(item); dependency != nullptr)
-					return dependency;
+				CHILLI_WARN("Failed to resolve dependency");
+				return nullptr;
 			}
-			CHILLI_WARN("Failed to resolve dependency {}", name);
-			return nullptr;
+			return std::static_pointer_cast<U>(m_dependencies.at(itr->second));
 		}
 
 		static void Add(const std::shared_ptr<SubSystem>& in)
 		{
 			m_dependencies.push_back(in);
+			m_cache.emplace(in->GetSystemType(), m_dependencies.size() - 1);
 		}
 
-		static void Add(const std::shared_ptr<SubSystem>& in, const char* name)
-		{
-			m_dependencies.push_back(in);
-			m_cache.emplace( name,m_dependencies.size() - 1 );
-		}
-
-		static bool Remove(const char* name)
+		static bool Remove(const std::shared_ptr<SubSystem>& in)
 		{
 			if (m_cache.size() < 1 || m_dependencies.size() < 1)
 				return false;
 
-			auto iterator = m_cache.find(name);
+			auto iterator = m_cache.find(in->GetSystemType());
 
 			if (iterator != m_cache.end())
 			{
@@ -66,9 +50,10 @@ namespace Engine::Core {
 		{
 			CHILLI_INFO("Dependencies flushed");
 			m_dependencies = {};
+			m_cache = {};
 		}
 	private:
 		static std::vector<std::shared_ptr<SubSystem>> m_dependencies;
-		static std::map<const char*, size_t> m_cache;
+		static std::map<int, size_t> m_cache;
 	};
 }
