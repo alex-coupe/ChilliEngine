f4d6a49d2727b95b2e809b4db37e8d67e9ec9ef4
Alex Coupe <alexander.coupe@dunnhumby.com>
2022-10-29 13:39:05 +0100

    Multiple lights coloured

--- Engine/Source/Rendering/shaders/Pixel_PhongCol.hlsl
+++ Engine/Source/Rendering/shaders/Pixel_PhongCol.hlsl
@@ -1,4 +1,6 @@
 #include "LightingFuncs.hlsli"
+#define MAX_POINTLIGHT_COUNT 4
+#define MAX_SPOTLIGHT_COUNT 4
 
 cbuffer objectBuffer : register(b0)
 {
@@ -7,46 +9,13 @@ cbuffer objectBuffer : register(b0)
     float shininess;
 }
 
-cbuffer dirLightBuffer : register(b1)
+cbuffer lightBuffer : register(b1)
 {
-    float3 dirlightDir;
-    float3 dirlightAmbient;
-    float3 dirlightDiffuse;
-    float3 dirlightSpecular;
+    lightCount lightcount;
+    dirLight dirlight;
+    pointLight pointlights[MAX_POINTLIGHT_COUNT];
+    spotLight spotlights[MAX_SPOTLIGHT_COUNT];
 };
-
-cbuffer pointLightBuffer : register(b2)
-{
-    float3 pointlightPos;
-    float3 pointlightAmbient;
-    float3 pointlightDiffuse;
-    float3 pointlightSpecular;
-    float pointlin;
-    float pointquadratic;
-    float pointconstant;
-    
-}
-
-cbuffer spotlightBuffer : register(b3)
-{
-    float3 spotPos;
-    float3 spotDir;
-    float3 spotAmbient;
-    float3 spotDiffuse;
-    float3 spotSpecular;
-    float spotlin;
-    float spotquadratic;
-    float spotconstant;
-    float spotcutOff;
-    float spotouterCutOff;
-}
-
-cbuffer lightDataBuffer : register(b4)
-{
-    int numDirLights;
-    int numPointLights;
-    int numSpotLights;
-}
 
 float4 main(float2 TexCoord : TexCoord, float4 pos : SV_Position, float3 normal : NORMAL, float3 worldPos : Position, float3 camPos : Position1) : SV_TARGET
 {
@@ -57,45 +26,16 @@ float4 main(float2 TexCoord : TexCoord, float4 pos : SV_Position, float3 normal 
     objMat.shininess = shininess;
     objMat.specular = objSpecular;
     
-    if (numDirLights > 0)
-    {
-        dirLight dirlight;
-        dirlight.direction = dirlightDir;
-        dirlight.ambient = dirlightAmbient;
-        dirlight.diffuse = dirlightDiffuse;
-        dirlight.specular = dirlightSpecular;
-        
+    if (lightcount.dirLightCount > 0)      
         result += CalcDirectionalLight(normal, dirlight, viewDir, objMat);
-    }
     
-    if (numPointLights > 0)
-    {
-        pointLight pointlight;
-        pointlight.position = pointlightPos;
-        pointlight.ambient = pointlightAmbient;
-        pointlight.diffuse = pointlightDiffuse;
-        pointlight.specular = pointlightSpecular;
-        pointlight.attlinear = pointlin;
-        pointlight.attconstant = pointconstant;
-        pointlight.attquadratic = pointquadratic;
-        
-        result += CalcPointLight(normal, pointlight, viewDir, worldPos, objMat);
-    }
-    if (numSpotLights > 0)
-    {
-        spotLight spotlight;
-        spotlight.position = spotPos;
-        spotlight.direction = spotDir;
-        spotlight.ambient = spotAmbient;
-        spotlight.diffuse = spotDiffuse;
-        spotlight.specular = spotSpecular;
-        spotlight.attlinear = spotlin;
-        spotlight.attconstant = spotconstant;
-        spotlight.attquadratic = spotquadratic;
-        spotlight.cutOff = spotcutOff;
-        spotlight.outerCutOff = spotouterCutOff;
-        
-        result += CalcSpotLight(normal, spotlight, viewDir, worldPos, objMat);
-    }
+    int pointLightCount = min(lightcount.pointLightCount, MAX_POINTLIGHT_COUNT);
+    for (int i = 0; i < pointLightCount; i++)  
+        result += CalcPointLight(normal, pointlights[i], viewDir, worldPos, objMat);
+  
+    int spotLightCount = min(lightcount.spotLightCount, MAX_SPOTLIGHT_COUNT);
+    for (int j = 0; j < spotLightCount; j++) 
+        result += CalcSpotLight(normal, spotlights[j], viewDir, worldPos, objMat);
+    
     return result;
 }
