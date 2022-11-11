2df1b3271e848fbd42e4b90d6954ec5c34ad3242
Alex Coupe <alexander.coupe@dunnhumby.com>
2022-11-11 13:56:25 +0000

    Splash panel done

--- ChilliEditor/Source/EditorPanels/SplashPanel.cpp
+++ ChilliEditor/Source/EditorPanels/SplashPanel.cpp
@@ -11,32 +11,49 @@ void Chilli::SplashPanel::DrawGui() const
 	if (ImGui::BeginPopupModal("Splashscreen", NULL, ImGuiWindowFlags_NoDecoration))
 	{
 		ImGui::PushFont(GuiManager::GetIO()->Fonts->Fonts[1]);
+		float center = ImGui::GetWindowWidth() / 2;
+		ImGui::SetCursorPosX(center-70.0f);
 		ImGui::Text("Chilli Engine");
 		ImGui::PopFont();
+		ImGui::Spacing();
+		ImGui::Spacing();
 		static char projectNameBuffer[50];
-		ImGui::TextColored(ImVec4(1.0f, 0.5f, 0.2f, 1.0f), "New Project");
-		ImGui::InputText("Project Name", projectNameBuffer,IM_ARRAYSIZE(projectNameBuffer));
+		ImGui::SetCursorPosX(center - 130.0f);
+		ImGui::TextColored(ImVec4(1.0f, 0.5f, 0.2f, 1.0f), "Name");
+		ImGui::SetCursorPosX(center - 130.0f);
+		ImGui::PushItemWidth(220.0f);
+		ImGui::InputText("##name", projectNameBuffer,IM_ARRAYSIZE(projectNameBuffer));
+		ImGui::PopItemWidth();
+		ImGui::Spacing();
 		ImGui::Spacing();
 		{
 			static nfdchar_t* outPath = NULL;
+			static char dirBuffer[256];
 			static nfdresult_t result = NFD_OKAY;
-			if (ImGui::Button("Project Directory"))
+
+			ImGui::SetCursorPosX(center - 130.0f);
+			ImGui::TextColored(ImVec4(1.0f, 0.5f, 0.2f, 1.0f), "Location");
+			ImGui::SetCursorPosX(center - 130.0f);
+			ImGui::PushItemWidth(220.0f);
+			ImGui::InputText("##location", dirBuffer, IM_ARRAYSIZE(dirBuffer));
+			ImGui::PopItemWidth();
+			ImGui::SameLine();
+			if (ImGui::Button("..."))
 			{
 				result = NFD_PickFolder(NULL, &outPath);
-			}
-
-			ImGui::SameLine();
-			ImGui::Text(outPath != NULL ? outPath : "Not Selected");
+				strcpy_s(dirBuffer, outPath);
+			}			
 			ImGui::Spacing();
-			if (ImGui::Button("Create Project") && projectNameBuffer[0] != NULL && outPath != NULL)
+			ImGui::SetCursorPosX(center - 70.0f);
+			if (ImGui::Button("Create") && projectNameBuffer[0] != NULL && outPath != NULL)
 			{
 				projMan->NewProject(projectNameBuffer, outPath);
 				free(outPath);
 				ImGui::CloseCurrentPopup();
 			}
 		}
-		ImGui::Separator();
-		ImGui::TextColored(ImVec4(1.0f, 0.5f, 0.2f, 1.0f), "Open Project");
+		
+		ImGui::SameLine();
 		{
 			static nfdchar_t* outPath = NULL;
 			static nfdresult_t result = NFD_OKAY;
